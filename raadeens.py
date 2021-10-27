be156894b9148e016b75268ba164cf6ac2726276
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-10-27 10:45:32 +0200

    werkt niet

--- raadeens.py
+++ raadeens.py
@@ -5,7 +5,7 @@ import time
 def clear():
     command = 'cls'
     os.system(command)
-
+clear()
 print('You have to guess a number. It ranges from 1 to 1000. You will have 10 tries per round, there are 20 rounds.')
 print('If you guess the number right, you\'ll get a point, if not, it will move on to the next round')
 #time.sleep(6)
